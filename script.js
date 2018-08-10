b5e3aca3e7504a1b505c99e60c113a5c98a3c950
Thomas Cohn <cohnt@umich.edu>
2018-08-10 13:52:33 -0400

    POINTS: Create point class with draw function.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 var epsilon = 10;
 var minClusterSize = 5;
+var colorRule = { unassigned: "black" };
 
 ///////////////////////////////////////////
 /// GLOBAL VARIABLES
@@ -16,7 +17,20 @@ var ctx;
 /// CLASSES
 ///////////////////////////////////////////
 
+function Point(loc) {
+	this.loc = loc.slice();
+	this.type = "unassigned";
+	this.clusterID = null;
 
+	this.draw = function() {
+		ctx.fillStyle = colorRule.unassigned;
+		ctx.beginPath();
+		ctx.moveTo(loc[0], loc[1]);
+		ctx.arc(loc[0], loc[1], 1, 0, 2*Math.PI, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+}
 
 ///////////////////////////////////////////
 /// FUNCTIONS
